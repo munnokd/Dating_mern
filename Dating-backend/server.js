3837b3249eb154a6c73b9ba5443b8fa226f9a5aa
munnokd <81804004+munnokd@users.noreply.github.com>
2021-06-20 09:30:13 +0530

    Add files via upload

--- Dating-backend/server.js
+++ Dating-backend/server.js
@@ -1,1 +1,48 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Cards from './dbCards.js'
+import Cors from 'cors'
+// App config
+const app=express();
+const port=process.env.PORT || 9000;
+const connection_url='mongodb+srv://munnokd:kalp972002@cluster0.bva6l.mongodb.net/tinderdb?retryWrites=true&w=majority'
+
+// Middlewares
+app.use(express.json())
+app.use(Cors());
+
+// DB config 
+mongoose.connect(connection_url,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+})
+
+// API Endpoints
+app.get('/',(req,res)=>res.status(200).send("Hello World!!!!!!!!!"));
+
+app.post('/tinder/cards',(req,res)=>{
+    const dbCard=req.body;
+
+    Cards.create(dbCard,(err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(201).send(data);
+        }
+    })
+})
+
+app.get('/tinder/cards',(req,res)=>{
+    Cards.find((err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(data);
+        }
+    })
+})
+
+// Listner
+app.listen(port,()=>console.log(`Listening on localhost:${port}`))
 
